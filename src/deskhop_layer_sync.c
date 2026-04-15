9e01da431c887616db95f69097d520c14ad9fd03
Charles Stein <charles.mst@gmail.com>
2026-04-14 23:00:47 -0300

    add optional debug key tap on DeskHop sync events

--- src/deskhop_layer_sync.c
+++ src/deskhop_layer_sync.c
@@ -3,7 +3,10 @@
 #include <zephyr/sys/util.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
+#include <zmk/hid.h>
+#include <zmk/endpoints.h>
 #include <zmk/keymap.h>
+#include <dt-bindings/zmk/hid_usage.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -25,6 +28,13 @@ static int deskhop_layer_sync_listener(const zmk_event_t *eh) {
 
     LOG_INF("DeskHop sync event indicators=0x%02x target=%d highest=%d", ev->indicators,
             target_layer, zmk_keymap_highest_layer_active());
+
+#if IS_ENABLED(CONFIG_ROBA_DESKHOP_LAYER_SYNC_DEBUG_KEY)
+    zmk_hid_keyboard_press((zmk_key_t)CONFIG_ROBA_DESKHOP_LAYER_SYNC_DEBUG_KEY_USAGE);
+    zmk_endpoints_send_report(HID_USAGE_KEY);
+    zmk_hid_keyboard_release((zmk_key_t)CONFIG_ROBA_DESKHOP_LAYER_SYNC_DEBUG_KEY_USAGE);
+    zmk_endpoints_send_report(HID_USAGE_KEY);
+#endif
 
     if (zmk_keymap_highest_layer_active() != target_layer) {
         zmk_keymap_layer_to(target_layer);
